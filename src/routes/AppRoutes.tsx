4af7c4ce4049e2844d24445319383a5ee49edb27
Sejoong Kim <sejoongkim@Sejoongui-MacBookPro.local>
2025-07-22 04:20:44 -0700

    fixing: 토큰 부분 디버깅중

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,10 @@
-import { Suspense } from "react";
-import { Route, Routes } from "react-router";
+import { Suspense, useContext, useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import AuthRoutes from "./AuthRoutes";
+
+import { UserContext } from "../context/UserInfoContext";
+
 import RegisterPage from "../pages/register/RegisterPage";
 import RegisterSuccessPage from "../pages/register/RegisterSuccessPage";
 import { LoginPage } from "../pages/login/loginPage";
@@ -10,18 +14,39 @@ import { ResetPasswordPage } from "../pages/find-info/resetPasswordPage";
 import { MainPage } from "../pages/main/MainPage";
 
 const AppRoutes = () => {
+  const { isLogin } = useContext(UserContext);
+
   return (
     <Suspense fallback={<div>loading...</div>}>
       <Routes>
         <Route path="/" element={<MainPage />} />
         {/* 인증 관련 라우팅 */}
         <Route element={<AuthRoutes />}>
-          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/register"
+            element={!isLogin ? <RegisterPage /> : <Navigate to="/" replace />}
+          />
           <Route path="/register-success" element={<RegisterSuccessPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/find" element={<FindInfoPage />} />
-          <Route path="/find-result" element={<FindResultPage />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route
+            path="/login"
+            element={!isLogin ? <LoginPage /> : <Navigate to="/" replace />}
+          />
+          <Route
+            path="/find"
+            element={!isLogin ? <FindInfoPage /> : <Navigate to="/" replace />}
+          />
+          <Route
+            path="/find-result"
+            element={
+              !isLogin ? <FindResultPage /> : <Navigate to="/" replace />
+            }
+          />
+          <Route
+            path="/reset-password"
+            element={
+              !isLogin ? <ResetPasswordPage /> : <Navigate to="/" replace />
+            }
+          />
         </Route>
       </Routes>
     </Suspense>
