b097ec7809c9aea440e501df28273809bc79574f
rukadeveloper <kansaki135@gmail.com>
2025-10-28 06:07:37 -0700

    fix: profile add

--- src/pages/my-pages/MyPage.tsx
+++ src/pages/my-pages/MyPage.tsx
@@ -1,5 +1,22 @@
+import MyPageProfile from "../../components/my-page/MyPageProfile";
+import { useUserInfo } from "../../stores/userStore";
+import styles from "./MyPage.module.scss";
+
 const MyPage = () => {
-  return <div></div>;
+  const { email, name, nickname, profileImage } = useUserInfo();
+
+  console.log(name);
+
+  return (
+    <div className={styles["mypage__wrapper"]}>
+      <MyPageProfile
+        name={name!}
+        email={email}
+        nickname={nickname!}
+        profileImage={profileImage}
+      />
+    </div>
+  );
 };
 
 export default MyPage;
