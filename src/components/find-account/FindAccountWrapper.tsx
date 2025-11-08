395a7dccd15ec11bf452464214f1e4e796348f3e
rukadeveloper <kansaki135@gmail.com>
2025-11-07 23:33:04 -0800

    delete find password

--- src/components/find-account/FindAccountWrapper.tsx
+++ src/components/find-account/FindAccountWrapper.tsx
@@ -4,7 +4,6 @@ import FindId from "./FindId";
 import { usePendingStore } from "../../stores/usePendingStore";
 import TextModal from "../common/text-modal/TextModal";
 import { useModalTextStore } from "../../stores/useModalText";
-import FindPassword from "./FindPassword";
 
 const FindAccountWrapper = () => {
   const tabs = ["ID/아이디", "PASS/비밀번호"];
@@ -30,7 +29,6 @@ const FindAccountWrapper = () => {
       </div>
       <div className={styles["find__account__wrapper__main"]}>
         {selectedTabs === "ID" && <FindId />}
-        {selectedTabs === "PASS" && <FindPassword />}
       </div>
       {modalPending && <TextModal text={modalText} usingConfirm={true} />}
     </div>
